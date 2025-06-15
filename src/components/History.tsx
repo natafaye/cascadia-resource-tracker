2df8338b8af713479b0367fb6b4f151c40661283
Natalie Childs <nata.faye@gmail.com>
2025-06-15 15:48:55 -0700

    fixed history going small

--- src/components/History.tsx
+++ src/components/History.tsx
@@ -14,12 +14,12 @@ export default function History({ history, resources }: Props) {
         resource: resources.find(r => r.name === item.name)!
     }))
     return (
-        <div className="bg-white p-4 flex gap-2 items-center h-14 mb-5">
-            <FaHistory className="text-neutral-400 text-3xl me-1" />
+        <div className="bg-white p-4 flex gap-2 items-center h-14 mb-5 overflow-hidden">
+            <FaHistory className="text-neutral-400 text-3xl me-1 shrink-0" />
             {historyWithResource.map(item => (
                 <ResourceIcon resource={item.resource} className={clsx(
                     item.amount === 1 ? "text-green-700" : "text-red-700",
-                    "text-3xl"
+                    "text-3xl shrink-0"
                 )}/>
             ))}
         </div>
