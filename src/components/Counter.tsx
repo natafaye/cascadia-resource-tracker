d6b51642ef58d1c58b0d07f7843e5d8419c18aaa
Natalie Childs <nata.faye@gmail.com>
2025-06-15 15:46:22 -0700

    enlarged buttons

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -19,18 +19,18 @@ export default function Counter({
                 <Component title={name} size={45} />
             </div>
             <div className="flex ms-4 border-2 border-white rounded-xl">
-                <div className="bg-white p-3 text-2xl text-black rounded-s-lg w-12 flex items-center justify-center">
+                <div className="bg-white p-3 text-3xl text-black rounded-s-lg w-12 flex items-center justify-center">
                     {tally}
                 </div>
-                <div className="flex flex-col gap-1 text-xl text-white p-1">
+                <div className="flex gap-1 text-4xl text-white p-1">
                     <button
-                        className={clsx("p-1 rounded-tr-lg", highlightClassName)}
+                        className={clsx("p-1", highlightClassName)}
                         onClick={() => updateTally(name, 1)}
                     >
                         <FaPlus />
                     </button>
                     <button
-                        className={clsx("p-1 rounded-br-lg", highlightClassName, "disabled:opacity-50")}
+                        className={clsx("p-1 rounded-e-lg", highlightClassName, "disabled:opacity-50")}
                         onClick={() => updateTally(name, -1)}
                         disabled={tally === 0}
                     >
